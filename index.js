ed504942579db06ad1e7539b6923cd76d1ae7784
renco88 <mdmahadihassanrenco@gmail.com>
2024-12-12 04:13:25 +0600

    add token

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
@@ -6,8 +8,12 @@ const app =express();
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wibzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,6 +34,22 @@ async function run() {
     const serviceCollection = client.db('cardoctor').collection('services');
     const bookingCollection= client.db('cardoctor').collection('booking');
 
+    // auth related api
+    app.post('/jwt', async(req,res)=>{
+      const user =req.body;
+      console.log(user);
+      const token =jwt.sign(user,process.env.ACCESS_SECRET,{expiresIn: '1h'})
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:'none'
+      })
+      .send({success: true})
+    })
+
+
+    //service relatade
     app.get('/services',async(req,res)=> {
         const cursor=serviceCollection.find();
         const result= await cursor.toArray();
@@ -49,6 +71,7 @@ async function run() {
 
      app.get('/booking',async(req,res)=>{
         console.log(req.query.email);
+        console.log('tok tok token', req.cookies.token);
         let query ={};
         if(req.query?.email){
             query={email: req.query.email}
